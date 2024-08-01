b6524749ff04995eafe9fdfcdd2fca76f4cb6c94
VladimirKuznetsov1990 <fixer2026@gmail.com>
2024-08-01 10:44:07 +0300

    add server

---
+++ server/eslint.config.mjs
@@ -0,0 +1,11 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import prettier from 'eslint-config-prettier';
+
+
+export default [
+  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
+  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  pluginJs.configs.recommended,
+  prettier,
+];
