e8a2242f08b361f24ab5d46e9a4db020cedab82f
VladimirKuznetsov1990 <fixer2026@gmail.com>
2024-08-01 14:32:44 +0300

    serverReCommit

--- server/db/models/orderitem.js
+++ server/db/models/orderitem.js
@@ -1,22 +1,23 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class OrderItem extends Model {
-    static associate({Order, Vinyl}) {
-      this.belongsTo(Order, {foreignKey: 'orderId'});
-      this.belongsTo(Vinyl, {foreignKey: 'vinylId'});
+    static associate({ Order, Vinyl }) {
+      this.belongsTo(Order, { foreignKey: 'orderId' });
+      this.belongsTo(Vinyl, { foreignKey: 'vinylId' });
     }
   }
-  OrderItem.init({
-    orderId: DataTypes.INTEGER,
-    vinylId: DataTypes.INTEGER,
-    quantity: DataTypes.INTEGER,
-    price: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'OrderItem',
-  });
+  OrderItem.init(
+    {
+      orderId: DataTypes.INTEGER,
+      vinylId: DataTypes.INTEGER,
+      quantity: DataTypes.INTEGER,
+      price: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: 'OrderItem',
+    },
+  );
   return OrderItem;
 };
