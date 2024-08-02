d4df72b54d01d51380303b75478e2b73756992a3
Fan-95 <Fantom2000@bk.ru>
2024-08-02 23:38:35 +0300

    loader-done-80%

--- client/src/components/hocs/Loader.tsx
+++ client/src/components/hocs/Loader.tsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import SpinnerUi from '../ui/SpinnerUi';
 
-export default function Loader({ showSpinner, children }: { showSpinner: boolean; children: React.ReactNode }): React.ReactNode | null {
-  if (showSpinner) {
-    return <SpinnerUi />;
+export default function Loader({
+  showSpinner,
+  children,
+}: {
+  showSpinner: boolean;
+  children: React.ReactNode;
+}): React.ReactNode | null {
+  const [isLoading, setIsLoading] = useState(showSpinner);
+
+  useEffect(() => {
+    if (showSpinner) {
+      setIsLoading(true);
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 1500); // 1-2 секунды
+    }
+  }, [showSpinner]);
+
+  if (isLoading) {
+    return (
+      <div>
+        <SpinnerUi id="record-canvas" labelColor="#00FF7F	" size={520} />
+      </div>
+    );
   }
   return children;
 }
