3a2e78db82b882728b6b7ee955be45d6d4ad477c
Fan-95 <Fantom2000@bk.ru>
2024-08-09 10:06:09 +0300

    done

--- client/src/components/pages/MainPage.tsx
+++ client/src/components/pages/MainPage.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useRef, useState } from 'react';
 import KeyboardDoubleArrowUpIcon from '@mui/icons-material/KeyboardDoubleArrowUp';
 import { useLocation } from 'react-router-dom';
 import '../style/styles.css';
-import { IconButton } from '@mui/material';
+import { IconButton, Typography } from '@mui/material';
 
 const scrollToElement = (elementId: string, offset: number = 0, duration: number = 1000): void => {
   const element = document.getElementById(elementId);
@@ -104,23 +104,31 @@ export default function MainPage(): JSX.Element {
           <div className="main-article__content">
             <h2 className="layers__title">Custom Vinyl</h2>
             <img style={{ width: '350px' }} src="/static/img/vinyl2.png" alt="" />
-            <p id="about" className="main-article__paragraph">
+            <Typography
+              sx={{ fontSize: { xs: '16px', sm: '18px' }, fontWeight: '500' }}
+              id="about"
+              className="main-article__paragraph"
+            >
               В нашем магазине вы найдете уникальные виниловые пластинки, которые мы красим и
               наносим на них изображения по вашему заказу. Также мы предлагаем услугу записи ваших
               любимых треков на винил. Создайте свою идеальную коллекцию с нами!
-            </p>
+            </Typography>
           </div>
         </article>
 
         <article className="main-article" style={{ backgroundImage: 'url(/static/img/fonVi.png)' }}>
           <div className="main-article__content">
             <h2 className="main-article__header">Наши работы:</h2>
-            <p id="our-works" className="main-article__paragraph">
+            <Typography
+              sx={{ fontSize: { xs: '16px', sm: '18px' }, fontWeight: '500', width: '100%'}}
+              id="our-works"
+              className="main-article__paragraph"
+            >
               Мы гордимся нашими работами и рады поделиться ими с вами. В этом разделе вы найдете
               примеры наших уникальных виниловых пластинок, которые мы красим и наносим на них
               изображения по вашему заказу. Также мы предлагаем услугу записи ваших любимых треков
               на винил. Создайте свою идеальную коллекцию с нами!
-            </p>
+            </Typography>
           </div>
           <div style={{ padding: '100px' }} className="gallery" ref={galleryRef}>
             {[0, 1, 2, 3, 4, 5].map((index) => (
@@ -147,11 +155,11 @@ export default function MainPage(): JSX.Element {
               bottom: '40px',
               left: '40px',
               zIndex: 1000,
-              boxShadow: "2px 2px 2px #00FFFF",
-              opacity: '.4'
+              boxShadow: '2px 2px 2px #00FFFF',
+              opacity: '.4',
             }}
           >
-            <KeyboardDoubleArrowUpIcon sx={{width: '40px', height: '40px', color: '#00FFFF'}}/>
+            <KeyboardDoubleArrowUpIcon sx={{ width: '40px', height: '40px', color: '#00FFFF' }} />
           </IconButton>
         )}
 
